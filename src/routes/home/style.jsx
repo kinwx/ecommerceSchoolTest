a3feccc6b86688f9eceb7ab49af8b9bbb9fc67ee
kinwx <davimessias26@gmail.com>
2024-01-22 15:19:26 -0300

    Continue Styled Home

--- src/routes/home/style.jsx
+++ src/routes/home/style.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 import banner from "../../assets/banner_background.jpg"
 
-export const SectionStyled = styled.section`
-    margin-top: 2em;
+export const SectionStyled1 = styled.section`
+    margin: 3em 0;
 `
 
 export const DivImageBackground = styled.div`
@@ -76,4 +76,84 @@ export const LinkStyled = styled.a`
     align-items: center;
     justify-content: center;
     gap: .2em;
+`
+
+// ===========
+export const FakeButtonFeatures = styled.div`
+    width: 10em;
+    margin: 0 auto;
+    padding: .6em 1em;
+
+    border: 1px solid black;
+    background-color: transparent;
+
+    font-weight: 500;
+    text-align: center;
+    pointer-events: none;
+`
+
+export const TitleFeatures = styled.h3`
+    text-align: center;
+    font-size: calc(10px + 3vmin);
+    font-weight: 500;
+    margin-top: .6em;
+    pointer-events: none;
+`
+
+export const LayoutFeaturesCards = styled.div`
+    margin-top: 2em;
+    display: flex;
+    gap: 1em;
+    pointer-events: none;
+`
+
+export const FeatureCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: .4em;
+    text-align: center;
+
+    & > p {
+        color: gray;
+    }
+`
+
+// ============
+export const StrongProducts = styled.strong`
+    font-size: calc(10px + 1vmin);
+`
+
+export const TitleProducts = styled.h2`
+    font-size: calc(10px + 3vmin);
+    font-weight: 600;
+`
+
+export const MenuIcons = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+    grid-auto-rows: 12em;
+    grid-gap: .5em;
+
+    & > div {
+        background-color: #f1f1f1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 2em;
+        transition: all .4s ease;
+
+        & > span {
+            font-size: calc(10px + 1vmin);
+            font-weight: 500;
+            pointer-events: none;
+        }
+
+        &:hover {
+            background-color: black;
+            color: white;
+        }
+    }
 `
